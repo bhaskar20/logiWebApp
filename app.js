2a27cee3af4dda61e7958d909ae5a80f4434f564
Bhaskar <sharmabhaskar13@gmail.com>
2015-12-18 02:49:01 +0530

    changed structure for better seperation of concerns

--- app.js
+++ app.js
@@ -5,16 +5,16 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+var routes = require('./server/routes/index');
 
 var app = express();
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname,'client','public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname,'client','public')));
 
 app.use('/', routes);
 
