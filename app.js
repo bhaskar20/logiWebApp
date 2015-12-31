1a4a9f38d8192609bf9ab3d478f06a6d1da5aa39
Bhaskar <sharmabhaskar13@gmail.com>
2015-12-31 23:57:31 +0530

    web launch

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./server/routes/index');
+var routes = require('./server/routes/index.js');
 
 var app = express();
 
@@ -16,38 +16,39 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname,'client','public')));
 
-app.use('/', routes);
+console.log("here in app.js");
+app.use('/',routes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
 
 // error handlers
 
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
+// if (app.get('env') === 'development') {
+//   app.use(function(err, req, res, next) {
+//     res.status(err.status || 500);
+//     res.render('error', {
+//       message: err.message,
+//       error: err
+//     });
+//   });
+// }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
+// app.use(function(err, req, res, next) {
+//   res.status(err.status || 500);
+//   res.render('error', {
+//     message: err.message,
+//     error: {}
+//   });
+// });
 
 module.exports = app;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile('./public/index.html');
+  res.sendFile(path.join(__dirname,'../../client/','index.html'));
 });
-
+router.get('/about',function(req,res,next){
+	res.sendFile(path.join(__dirname,'../../client/','about.html'));
+});
+router.get('/contact',function(req,res,next){
+	res.sendFile(path.join(__dirname,'../../client/','contact.html'));
+});
+router.get('/services',function(req,res,next){
+	res.sendFile(path.join(__dirname,'../../client/','services.html'));
+});
+// router.get('/login',function(req,res,next){
+// 	var options = {
+//     root: __dirname + '../../client/',
+//     dotfiles: 'deny',
+//     headers: {
+//         'x-timestamp': Date.now(),
+//         'x-sent': true
+//     }
+//   };
+// 	res.sendFile(path.join(__dirname,'../../client/','login.html'));
+// });
+router.get('/*',function(req,res,next){
+    res.redirect('/');
+});
 module.exports = router;
